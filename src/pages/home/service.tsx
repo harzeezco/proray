3b5dd6ace312ad5042293d42fb1b82688cbed84b
webwithyaroo <olawaleridollahi@gmail.com>
2024-11-13 06:32:20 -1100

    updated service file

--- src/pages/home/service.tsx
+++ src/pages/home/service.tsx
@@ -15,53 +15,52 @@ const ServiceSection = () => (
       Our Key Services
     </h1>
 
-    <div className='mx-auto  grid max-w-5xl grid-cols-10 gap-6 p-6 max-[831px]:grid-cols-1'>
+    <div className='mx-auto  grid max-w-5xl grid-cols-11 gap-6 p-6 max-[831px]:grid-cols-1'>
       {/* Service 1 */}
+
       <motion.article
-        aria-labelledby='hr-training'
+        aria-labelledby='equipment-servicing'
         className='flex items-start justify-start gap-5 rounded-lg bg-blue-400 p-6  max-sm:gap-2 max-sm:p-4 min-[831px]:col-span-6 min-[831px]:justify-center'
-        initial={{ opacity: 0, y: 20 }} // Start with hidden opacity and slight upward position
-        transition={{ duration: 0.5, ease: 'easeInOut' }} // Animation duration
-        viewport={{ once: true }} // Only animate once when in view
-        whileInView={{ opacity: 1, y: 0 }} // Animate to visible and original position
+        initial={{ opacity: 0, y: 20 }}
+        transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.6 }}
+        viewport={{ once: true }}
+        whileInView={{ opacity: 1, y: 0 }}
       >
         <Image
-          alt='Human Resources icon'
+          alt='Equipment Servicing icon'
           height={30}
-          src='/icon/radio-wave.svg'
+          src='/icon/heart.svg'
           width={30}
         />
         <div>
           <h2
             className='text-xl font-semibold text-gray-200'
-            id='hr-training'
+            id='equipment-servicing'
           >
-            Human Resources and Training
+            Equipment Servicing and Management
           </h2>
           <p className='font-bold text-primary'>
-            We offer comprehensive recruitment and training solutions
-            across Nigeria, including:
+            We offer tailored maintenance and support for your
+            equipment:
           </p>
-          <ul className='list-disc pl-5 text-sm text-[#677182] '>
-            <li>Full recruitment outsourcing services</li>
+          <ul className='list-disc pl-5 text-sm text-[#677182]'>
+            <li>Customized maintenance plans</li>
             <li>
-              Develop recruitment strategies for local and
-              international candidates
+              First-level support with expert biomedical engineers
             </li>
             <li>
-              Training programs with UK-based radiographers (Brain
-              Gain)
+              Regular preventative maintenance to reduce downtime
             </li>
-            <li>Apprenticeship programs to support local talent</li>
-            <li>Performance management frameworks</li>
+            <li>
+              Service contracts for optimal equipment performance
+            </li>
           </ul>
         </div>
       </motion.article>
-
       {/* Service 2 */}
       <motion.article
         aria-labelledby='project-management'
-        className='flex items-start justify-start gap-5 rounded-lg bg-blue-400 p-6  max-sm:gap-2 max-sm:p-4 min-[831px]:col-span-4 min-[831px]:justify-center'
+        className='flex items-start justify-start gap-5 rounded-lg bg-blue-400 p-6  max-sm:gap-2 max-sm:p-4 min-[831px]:col-span-5 min-[831px]:justify-center'
         initial={{ opacity: 0, y: 20 }}
         transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.2 }}
         viewport={{ once: true }}
@@ -129,41 +128,42 @@ const ServiceSection = () => (
 
       {/* Service 4 */}
       <motion.article
-        aria-labelledby='equipment-servicing'
-        className='flex items-start justify-start gap-5 rounded-lg bg-blue-400 p-6  max-sm:gap-2 max-sm:p-4 min-[831px]:col-span-5 min-[831px]:justify-center'
-        initial={{ opacity: 0, y: 20 }}
-        transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.6 }}
-        viewport={{ once: true }}
-        whileInView={{ opacity: 1, y: 0 }}
+        aria-labelledby='hr-training'
+        className='flex items-start justify-start gap-5 rounded-lg bg-blue-400 p-6  max-sm:gap-2 max-sm:p-4 min-[831px]:col-span-6 min-[831px]:justify-center'
+        initial={{ opacity: 0, y: 20 }} // Start with hidden opacity and slight upward position
+        transition={{ duration: 0.5, ease: 'easeInOut' }} // Animation duration
+        viewport={{ once: true }} // Only animate once when in view
+        whileInView={{ opacity: 1, y: 0 }} // Animate to visible and original position
       >
         <Image
-          alt='Equipment Servicing icon'
+          alt='Human Resources icon'
           height={30}
-          src='/icon/heart.svg'
+          src='/icon/radio-wave.svg'
           width={30}
         />
         <div>
           <h2
             className='text-xl font-semibold text-gray-200'
-            id='equipment-servicing'
+            id='hr-training'
           >
-            Equipment Servicing and Management
+            Human Resources and Training
           </h2>
           <p className='font-bold text-primary'>
-            We offer tailored maintenance and support for your
-            equipment:
+            We offer comprehensive recruitment and training solutions
+            across Nigeria, including:
           </p>
-          <ul className='list-disc pl-5 text-sm text-[#677182]'>
-            <li>Customized maintenance plans</li>
+          <ul className='list-disc pl-5 text-sm text-[#677182] '>
+            <li>Full recruitment outsourcing services</li>
             <li>
-              First-level support with expert biomedical engineers
+              Develop recruitment strategies for local and
+              international candidates
             </li>
             <li>
-              Regular preventative maintenance to reduce downtime
+              Training programs with UK-based radiographers (Brain
+              Gain)
             </li>
-            <li>
-              Service contracts for optimal equipment performance
-            </li>
+            <li>Apprenticeship programs to support local talent</li>
+            <li>Performance management frameworks</li>
           </ul>
         </div>
       </motion.article>
--- src/pages/home/showcase.tsx
+++ src/pages/home/showcase.tsx
@@ -3,8 +3,8 @@
 import { motion } from 'framer-motion';
 
 const ShowcaseSection = () => (
-  <section className='mt-20 flex min-h-screen w-full items-center justify-center bg-banner-img bg-cover bg-center bg-no-repeat py-20'>
-    <div className='showcase container  h-fit border-slate-400 py-10 lg:px-3 xl:border '>
+  <section className='mt-20 flex min-h-screen w-full items-end justify-center bg-banner-img bg-cover bg-center bg-no-repeat pb-10 pt-20'>
+    <div className='showcase container  h-fit border-slate-400 py-10 lg:border lg:px-3 '>
       <div className='container '>
         <motion.h1
           className='mx-auto max-w-[900px]  text-center text-4xl md:text-5xl lg:text-6xl'
@@ -15,7 +15,7 @@ const ShowcaseSection = () => (
         >
           Leading the Way in Healthcare Safety and Efficiency
         </motion.h1>
-        <div className='mt-10 grid grid-cols-3 items-center justify-center gap-5 text-balance rounded-lg border-2 border-white border-opacity-50 bg-blue-200 px-8 py-4 max-md:grid-cols-1'>
+        <div className='mt-10 grid grid-cols-3 items-center justify-center gap-5 text-balance rounded-lg border-2 border-white border-opacity-50 bg-[#19446A] px-8 py-4 max-md:grid-cols-1'>
           <motion.div
             className='border-white border-opacity-25 max-md:border-b-2 md:border-r'
             initial={{ opacity: 0, y: 20 }}
@@ -24,7 +24,7 @@ const ShowcaseSection = () => (
             whileInView={{ opacity: 1, y: 0 }}
           >
             <h1 className='pb-2 text-3xl font-bold'>100+</h1>
-            <p className='max-md:pb-4'>
+            <p className='font-thin  max-md:pb-4'>
               Hospitals equipped with advanced radiation protection
               solutions.
             </p>
